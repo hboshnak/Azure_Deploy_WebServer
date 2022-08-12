8aebbe721cc3222cd839415cc598e54191cb7348
Hubenix <hubenix@gmail.com>
2022-08-12 13:12:40 +0300

    Task: modify the network interface

--- main.tf
+++ main.tf
@@ -6,26 +6,17 @@ provider "azurerm" {
     tenant_id       = "${var.tenant_id}"
 }
 
+// Resource group
 data "azurerm_resource_group" "main" {
     name     = "Azuredevops"
 }
 
+// Virtual network
 resource "azurerm_virtual_network" "main" {
     name                    = "${var.prefix}-network"
     address_space           = ["10.0.0.0/22"]
     location                = azurerm_resource_group.main.location
     resource_group_name     = azurerm_resource_group.main.name
-    tags = {
-        project: "udacityP1"
-    }
-}
-
-// Virtual network
-resource "azurerm_network_interface" "main" {
-    name                    = "${var.prefix}-network"
-    address_space       = ["10.0.0.0/22"]
-    location            = data.azurerm_resource_group.main.location
-    resource_group_name = data.azurerm_resource_group.main.name  
     tags = {
         project: "udacityP1"
     }
@@ -78,6 +69,22 @@ resource "azurerm_network_security_rule" "rule2" {
     destination_address_prefix = "10.0.0.0/24"  
     resource_group_name         = data.azurerm_resource_group.main.name
     network_security_group_name = azurerm_network_security_group.main.name
+}
+
+// Network interface
+resource "azurerm_network_interface" "main" {
+    name                = "${var.prefix}-network-interface${count.index}"
+    location            = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+    count               = var.number_of_virtual_machines
+      ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    }  
+    tags = {
+        project: "udacityP1"
+    }  
 }
 
 resource "azurerm_public_ip" "main" {
